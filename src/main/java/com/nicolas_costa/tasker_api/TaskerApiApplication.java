ca8cdf44a626adb2b57716685006d86bcb3de59d
Nícolas Costa <nicolas.nico@hotmail.com>
2022-02-20 19:05:30 -0300

    feat: initial commit

---
+++ src/main/java/com/nicolas_costa/tasker_api/TaskerApiApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas_costa.tasker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicolas_costa/tasker_api/TaskerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas_costa.tasker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
